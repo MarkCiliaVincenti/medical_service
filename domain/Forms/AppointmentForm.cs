d5d95d17da161ea342603c1e439ca9650078f600
fs.anvr <fs.anvr@gmail.com>
2022-10-18 19:09:34 +1000

    added appointment form fields

--- domain/Forms/AppointmentForm.cs
+++ domain/Forms/AppointmentForm.cs
@@ -5,11 +5,15 @@ public class AppointmentForm
     public int PatientID { get; set; }
     public int DoctorID { get; set; }
     public DateOnly Date { get; set; }
+    public TimeOnly Start { get; set; }
+    public TimeOnly End { get; set; }
 
-    public AppointmentForm(int patientID, int doctorID, DateOnly date)
+    public AppointmentForm(int patientID, int doctorID, DateOnly date, TimeOnly start, TimeOnly end)
     {
         PatientID = patientID;
         DoctorID = doctorID;
         Date = date;
+        Start = start;
+        End = end;
     }
 }
