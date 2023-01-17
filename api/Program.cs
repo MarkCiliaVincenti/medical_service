ccece9742c92495e0535367f93f542c56d1146eb
fs.anvr <fs.anvr@gmail.com>
2023-01-17 16:32:44 +1000

    added dependencies for the database

--- api/Program.cs
+++ api/Program.cs
@@ -1,6 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+
+using Domain;
+using Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ServiceContext>(options =>
+options.UseSqlite($"Host=localhost;Port=5000;Database=repo;Username=fsanvr;Password=12345"));
+
+builder.Services.AddTransient<IUserRepository, UserRepositoryImpl>();
+builder.Services.AddTransient<UserService>();
+
+builder.Services.AddTransient<IDoctorRepository, DoctorRepositoryImpl>();
+builder.Services.AddTransient<DoctorService>();
+
+builder.Services.AddTransient<IAppointmentRepository, AppointmentRepositoryImpl>();
+builder.Services.AddTransient<AppointmentService>();
+
+builder.Services.AddTransient<IScheduleRepository, ScheduleRepositoryImpl>();
+builder.Services.AddTransient<ScheduleService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
