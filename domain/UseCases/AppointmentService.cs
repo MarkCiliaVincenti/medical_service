07fea3b539f49ef946b97b7cde05e13597a21081
fs.anvr <fs.anvr@gmail.com>
2023-01-19 14:20:05 +1000

    blocking appointments by DoctorID

--- domain/UseCases/AppointmentService.cs
+++ domain/UseCases/AppointmentService.cs
@@ -3,11 +3,22 @@ namespace Domain;
 public class AppointmentService
 {
     public readonly IAppointmentRepository _repository;
-    private static SemaphoreSlim appointmentSemaphore = new SemaphoreSlim(1, 1);
+    private static Dictionary<int, SemaphoreSlim> semaphoreByDoctor = new Dictionary<int, SemaphoreSlim>();
 
     public AppointmentService(IAppointmentRepository repository)
     {
         _repository = repository;
+    }
+
+    private SemaphoreSlim GetOrCreate(IDictionary<int, SemaphoreSlim> dict, int key)
+    {
+        if (!dict.TryGetValue(key, out SemaphoreSlim? val))
+        {
+            val = new SemaphoreSlim(1, 1);
+            dict.Add(key, val);
+        }
+
+        return val;
     }
     
     async public Task<Result<Appointment>> CreateAppointment(AppointmentForm form)
@@ -26,13 +37,14 @@ public class AppointmentService
         Appointment? appointment = null;
         try
         {
-            await appointmentSemaphore.WaitAsync();
+            await GetOrCreate(semaphoreByDoctor, form.DoctorID).WaitAsync();
 
             appointment = await _repository.CreateAppointment(form);
         }
         finally
         {
-            appointmentSemaphore.Release();
+            semaphoreByDoctor[form.DoctorID].Release();
+            semaphoreByDoctor.Remove(form.DoctorID);
         }
 
         if (appointment is not null)
@@ -46,39 +58,27 @@ public class AppointmentService
         if (string.IsNullOrEmpty(specialization))
             return Result.Err<List<(DateTime, DateTime)>>("Specialization not specified");
         
+        var result = _repository.GetAllDates(specialization, date);
+
+        var start = date.ToDateTime(new TimeOnly(0, 0, 0));
+        var end = date.ToDateTime(new TimeOnly(23, 59, 59));
         var freeDates = new List<(DateTime, DateTime)>();
-        try
+        var lastDate = (start, start);
+
+        var busyDates = await result;
+        if (busyDates.Count == 0)
         {
-            await appointmentSemaphore.WaitAsync();
-
-            var result = _repository.GetAllDates(specialization, date);
-
-            var start = date.ToDateTime(new TimeOnly(0, 0, 0));
-            var end = date.ToDateTime(new TimeOnly(23, 59, 59));
-            //var freeDates = new List<(DateTime, DateTime)>();
-            var lastDate = (start, start);
-
-            var busyDates = await result;
-            if (busyDates.Count == 0)
-            {
-                appointmentSemaphore.Release();
-
-                return Result.Ok(new List<(DateTime, DateTime)>{(start, end)});
-            }
-
-            foreach(var currentDate in busyDates)
-            {
-                freeDates.Add((lastDate.Item2, currentDate.Item1));
-                lastDate = currentDate;
-            }
-
-            if (busyDates.Last().Item2 != end)
-                freeDates.Add((busyDates.Last().Item2, end));
+            return Result.Ok(new List<(DateTime, DateTime)>{(start, end)});
         }
-        finally
+
+        foreach(var currentDate in busyDates)
         {
-            appointmentSemaphore.Release();
+            freeDates.Add((lastDate.Item2, currentDate.Item1));
+            lastDate = currentDate;
         }
+
+        if (busyDates.Last().Item2 != end)
+            freeDates.Add((busyDates.Last().Item2, end));
 
         return Result.Ok<List<(DateTime, DateTime)>>(freeDates);
     }
